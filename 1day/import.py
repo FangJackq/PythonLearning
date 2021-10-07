c36683e5d6a59a5dc8cd17e47d063a0962170697
FangJackq <921266951@qq.com>
2021-10-07 22:31:33 +0800

    first day learn python

---
+++ 1day/import.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2021-10-05 19:24:09
+# @Author  : Thanks for Yong Lee Tell me how to build this Package (${email})
+# @Link    : ${link}
+# @Version : $Id$
+
+import this
